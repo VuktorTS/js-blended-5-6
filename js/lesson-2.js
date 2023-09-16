9b51dd2fdfb5b8947999765da834b4a098eda90f
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-09-16 15:26:25 +0300

    khrystyna

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -74,3 +74,51 @@
 // console.log(calculator);
 // console.log(`sum`, calculator.sum());
 // console.log(`mult`, calculator.mult());
+
+// ЗАДАЧА 1
+// Напишіть дві функції:
+// letMeSeeYourName(callback) - запитує імя користувача , через prompt і викликає колбек функцію (callback)
+// greet(name) - колбек , який приймає імя і логінує в консоль рядок  "Привіт" + імя
+
+// const greet = (userName) => console.log(`Привіт ${userName}`);
+
+// function letMeSeeYourName(callback) {
+//   const userName = prompt("Ваше ім'я?");
+//   return callback(userName);
+// }
+
+// letMeSeeYourName(greet);
+
+// ЗАДАЧА 2
+// Напишіть дві функції:
+// makeProduct(name, price, callback) -приймає імя та ціну товару, а також колбек.Функція створює обєкт товару, добавляє йому унікальний ідентифікатор, в значення id і викликає колбек, передаючи йому створений обєкт.
+// showProduct(product) - колбек приймає обєкт продукта і логінує його в консоль;
+
+// function makeProduct(name, price, callback) {
+//   const product = {
+//     name,
+//     price,
+//     id: Date.now(),
+//   };
+//   return callback(product);
+// }
+// function showProduct(product) {
+//   console.log(product);
+// }
+
+// makeProduct("Холодильник", 10000, showProduct);
+
+// ЗАДАЧА 3
+// Сортування примітивів.
+// Виконати сортування масиву цін зі спадання та зростання.
+// const prices = [2, 14, 1, 37, 26, 8];
+
+// const descend = [...prices].sort((a, b) => b - a);
+// console.log(descend);
+
+// ЗАДАЧА 4
+// Виконати сортування масиву назв моніторів в алфавітному та зворотному алфавітному порядку.
+// const items = ["SAMSUNG", "LG", "ASUS", "DELL", "BENQ"];
+
+// const sorted = [...items].sort((a, b) => a.localeCompare(b));
+// console.log(sorted);
