9b5baee1322cea3de7ed93ae6c39e4503bc14a10
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-09-30 16:07:04 +0300

    task3

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -70,33 +70,66 @@
 // Добавте метод getInfo(), який повертає рядок:
 // User ${ імя } is ${ вік } years old and has ${ кількість постів } posts.
 
-class User {
-  constructor({ username, age, numberOfPost }) {
-    this.username = username;
-    this.age = age;
-    this.numberOfPost = numberOfPost;
+// class User {
+//   constructor({ username, age, numberOfPost }) {
+//     this.username = username;
+//     this.age = age;
+//     this.numberOfPost = numberOfPost;
+//   }
+//   getInfo() {
+//     return `User ${this.username} is ${this.age} years old and has ${this.numberOfPost} posts.`;
+//   }
+// }
+
+// const user = new User({ username: "Victor", age: 37, numberOfPost: 6 });
+
+// console.log(user);
+// console.log(user.getInfo());
+
+// class NewUser extends User {
+//   constructor({ username, age, numberOfPost }) {
+//     super({ username, age, numberOfPost });
+//     this.username = username;
+//   }
+//   getUser() {
+//     return `My name ${this.username}`;
+//   }
+// }
+
+// const newUser = new NewUser({ username: "Poly", age: 30, numberOfPost: 1 });
+
+// console.log(newUser);
+// console.log(newUser.getUser());
+// ЗАДАЧА 3
+// Напиши клас Storage який створює об'єкти для керування складом товарів.
+// При виклику отримуватиме один аргумент - початковий масив товарів, і записуватиме його у властивість items.
+// Добавте методи класу:
+
+// getItems() - повертає масив товарів.
+// addItem(item) - отримує новий товар та додає його до поточних.
+// removeItem(item) - отримає товар і, якщо він є, видаляє його з поточних.
+
+class Storage {
+  constructor(arr) {
+    this.arr = arr;
   }
-  getInfo() {
-    return `User ${this.username} is ${this.age} years old and has ${this.numberOfPost} posts.`;
+  getItems() {
+    return this.arr;
+  }
+  addItem(item) {
+    this.arr.push(item);
+  }
+  removeItem(item) {
+    const itemIdx = this.arr.indexOf(item);
+    if (itemIdx === -1) {
+      return;
+    } else {
+      this.arr.splice(itemIdx, 1);
+    }
   }
 }
 
-const user = new User({ username: "Victor", age: 37, numberOfPost: 6 });
-
-console.log(user);
-console.log(user.getInfo());
-
-class NewUser extends User {
-  constructor({ username, age, numberOfPost }) {
-    super({ username, age, numberOfPost });
-    this.username = username;
-  }
-  getUser() {
-    return `My name ${this.username}`;
-  }
-}
-
-const newUser = new NewUser({ username: "Poly", age: 30, numberOfPost: 1 });
-
-console.log(newUser);
-console.log(newUser.getUser());
+const items = new Storage(["Виноград", "Банани", "Кокоси", "Ківі"]);
+items.addItem("Манго");
+items.removeItem("Ківі");
+console.log(items);
