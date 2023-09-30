8859380d47101f1d70af5627e95b45cc2203ee6f
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-09-30 16:49:26 +0300

    16:50

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -203,3 +203,81 @@
 //   action.call(product);
 // }
 // callAction(product.showPrice);
+// ЗАДАЧА 4
+// ПОТРІБНО СТВОРИТИ Ф-ЦІЮ , ЯКА БУДЕ ШУКАТИ НАРЦИСА.ГОЛОВНА УМОВА НАРЦИСА ЗНАЮТЬ УСІ , НАРЦИС НЕ ЗНАЄ НІКОГО
+// є елемент масиву , який незнає нікого
+// якщо не одна така людина , то вертаємо not found
+// якщо один , то перевірити , чи його знають усі люди, якщо так то вертаємо found
+
+const people1 = [
+  {
+    name: "Alex",
+    know: ["Eva", "Jhon"],
+  },
+  {
+    name: "Ivan",
+    know: ["Jhon", "Alex"],
+  },
+  {
+    name: "Eva",
+    know: ["Alex", "Jhon"],
+  },
+  {
+    name: "Jhon",
+    know: [],
+  },
+];
+const people2 = [
+  {
+    name: "Alex",
+    know: ["Eva", "Jhon"],
+  },
+  {
+    name: "Jhon",
+    know: ["Eva"],
+  },
+  {
+    name: "Eva",
+    know: [],
+  },
+  {
+    name: "Ivan",
+    know: ["Jhon", "Alex"],
+  },
+];
+
+const people3 = [
+  {
+    name: "Alex",
+    know: ["Eva", "Jhon"],
+  },
+  {
+    name: "Jhon",
+    know: [],
+  },
+  {
+    name: "Eva",
+    know: ["Alex", "Jhon"],
+  },
+  {
+    name: "Ivan",
+    know: ["Jhon", "Alex"],
+  },
+];
+
+function findNarcis(peoples) {
+  const arr = peoples.filter((people) => people.know.length === 0);
+  const frend = arr[0].name;
+  const isNarcis = peoples.every(
+    (people) => people.know.includes(frend) || people.name === frend
+  );
+
+  if (arr.length === 0) {
+    return "not found";
+  }
+  return isNarcis ? frend : "not found";
+}
+
+console.log(findNarcis(people1));
+console.log(findNarcis(people2));
+console.log(findNarcis(people3));
